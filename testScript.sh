44fbe183a8faa3f55ee29f2ff17208567120fe2a
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-10-25 10:23:32 +0330

    change the print order of operators and some of the tests

--- testScript.sh
+++ testScript.sh
@@ -2,14 +2,16 @@ export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
 rm *.class
 rm *.tokens
 rm our_smoola*.java
-java -jar /usr/local/lib/antlr-4.7.1-complete.jar our_smoola.g4
+java -jar /usr/local/lib/antlr-4.7.1-complete.jar our_smoola_2.g4
 javac our_smoola*.java
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/10.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/2.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/3.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/4.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/5.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/6.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/7.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/8.txt
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/9.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/1.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/2.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/3.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/4.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/5.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/6.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/7.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/8.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/9.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/10.txt
+java org.antlr.v4.gui.TestRig our_smoola_2 program -gui < ./Tests/11.txt
