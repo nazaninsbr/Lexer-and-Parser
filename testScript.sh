946378d8a77353cf80e33a3a29b4386c1ee2a254
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-10-25 19:26:08 +0330

    no errors on any tests so far

--- testScript.sh
+++ testScript.sh
@@ -4,5 +4,33 @@ rm *.tokens
 rm our_smoola*.java
 java -jar /usr/local/lib/antlr-4.7.1-complete.jar our_smoola.g4
 javac our_smoola*.java
+echo 1
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/1.txt
+echo 2
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/2.txt
+echo 3
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/3.txt
+echo 4
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/4.txt
+echo 5
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/5.txt
+echo 6
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/6.txt
+echo 7
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/7.txt
+echo 8
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/8.txt
+echo 9
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/9.txt
+echo 10
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/10.txt
+echo 11
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/11.txt
+echo 12
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/12.txt
+echo 13
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/13.sml
+echo 14
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/14.sml
 echo 15
-java org.antlr.v4.gui.TestRig our_smoola program -gui < ./Tests/15.sml
+java org.antlr.v4.gui.TestRig our_smoola program < ./Tests/15.sml
