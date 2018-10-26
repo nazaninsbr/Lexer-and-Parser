c36a39439d045652d9396bb3387d48b5de02b931
Marzieh <marziehrezaie2@gmail.com>
2018-10-26 19:03:28 +0330

    Update testScript.bat

--- testScript.bat
+++ testScript.bat
@@ -1,23 +1,23 @@
 del *.class
 del *.tokens
-del 810194318_810194346*.java
-java org.antlr.v4.Tool 810194318_810194346.g4
-javac 810194318_810194346*.java
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/1.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/2.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/3.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/4.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/5.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/6.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/7.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/8.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/9.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/10.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/11.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/12.txt
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/13.sml
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/14.sml
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/15.sml
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/16.sml
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/17.sml
-java org.antlr.v4.runtime.misc.TestRig 810194318_810194346 program -gui < ./Tests/18.sml
+del our_smoola*.java
+java org.antlr.v4.Tool our_smoola.g4
+javac our_smoola*.java
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/1.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/2.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/3.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/4.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/5.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/6.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/7.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/8.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/9.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/10.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/11.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/12.txt
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/13.sml
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/14.sml
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/15.sml
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/16.sml
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/17.sml
+java org.antlr.v4.runtime.misc.TestRig our_smoola program -gui < ./Tests/18.sml
